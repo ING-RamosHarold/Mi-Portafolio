b95b85291d052887057b91864e4b237731a87b75
Harold Ramos Salazar <137670994+ING-RamosHarold@users.noreply.github.com>
2024-11-16 20:19:26 -0500

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer'>
-            Harold &nbsp; <span>| Programador Z</span>
+            Harold &nbsp; <span></span>
           </p>
         </Link>
 
